0ba19d4b00b99207ef74cd0ff6dd299779d7bc94
Shamoev <wamoev@mail.ru>
2017-03-08 11:59:20 +0200

    Project homeworks was created

---
+++ src/edu/sourceit/homeworks/homework2/ClockAngle.java
@@ -0,0 +1,13 @@
+package edu.sourceit.homeworks.homework2;
+
+public class ClockAngle {
+    public static void main(String[] args) {
+        int hours = 2;
+        int mins = 30;
+        int result = 0;
+
+        /*Your code here*/
+
+        System.out.println("Angle between hours and minute narrows is " + result);
+    }
+}
